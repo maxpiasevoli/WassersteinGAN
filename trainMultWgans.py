d7866e8fcd795e0876e0d3af60d7e7ab2849dcd3
maxpiasevoli <maxpiasevoli@gmail.com>
2020-03-30 01:24:49 -0400

    debugging automated script

--- trainMultWgans.py
+++ trainMultWgans.py
@@ -29,9 +29,7 @@ module purge
 module load anaconda3
 conda activate torch-gpu
 
-srun python main.py --dataset {0} --dataroot ./ --imageSize {1} --nc 1 --ngf
- 64 --ndf 64 --niter {2} --experiment /scratch/gpfs/mp16 --experiment_name {3}
- --cuda --ngpu 4 --batchSize 20
+srun python main.py --dataset {0} --dataroot ./ --imageSize {1} --nc 1 --ngf 64 --ndf 64 --niter {2} --experiment /scratch/gpfs/mp16 --experiment_name {3} --cuda --ngpu 4 --batchSize 20
 '''
 
 for i in range(int(opt.num_models)):
