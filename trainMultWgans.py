4411268ab92e97ab8558778e6ae32ded70ed6655
Max Piasevoli <maxpiasevoli@gmail.com>
2020-04-14 12:03:37 -0400

    modified trainMultWgans to use 2 gpus and require experiment parameter

--- trainMultWgans.py
+++ trainMultWgans.py
@@ -1,6 +1,8 @@
 # As of now, this file only generates WGANs with the convolutional architecture
 # but it can easily be changed to automate training of mlp WGANs
-# python trainMultWgans.py --dataset behavioral --imageSize 32 --niter 50000 --num_models 30
+# python trainMultWgans.py --dataset behavioral --imageSize 32 --niter 50000 --num_models 30 --experiment
+# if using the tiger gpu, experiment should be in /scratch/gpfs/*username*
+# if using the adroit gpu, experiment should be in /scratch/network/*username*/
 
 import argparse
 import subprocess
@@ -10,6 +12,7 @@ parser.add_argument('--dataset', required=True, help='cifar10 | lsun | imagenet 
 parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
 parser.add_argument('--niter', type=int, default=25, help='number of epochs to train for')
 parser.add_argument('--num_models', default=30, help='Number of WGAN models to train')
+parser.add_argument('--experiment', required=True, help='tag for experiment names')
 opt = parser.parse_args()
 
 
@@ -20,7 +23,7 @@ slurm_script = '''#!/bin/bash
 #SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G per cpu-core is default)
 
-#SBATCH --gres=gpu:4             # number of gpus per node
+#SBATCH --gres=gpu:2             # number of gpus per node
 #SBATCH --time=24:00:00          # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=all          # send email on job start, end and fail
 #SBATCH --mail-user=mp16@princeton.edu
@@ -29,7 +32,7 @@ module purge
 module load anaconda3
 conda activate torch-gpu
 
-srun python main.py --dataset {0} --dataroot ./ --imageSize {1} --nc 1 --ngf 64 --ndf 64 --niter {2} --experiment /scratch/gpfs/mp16 --experiment_name {3} --cuda --ngpu 4 --batchSize 20 --auto_number {4}
+srun python main.py --dataset {0} --dataroot ./ --imageSize {1} --nc 1 --ngf 64 --ndf 64 --niter {2} --experiment {3} --experiment_name {4} --cuda --ngpu 2 --batchSize 20 --auto_number {5}
 '''
 
 for i in range(int(opt.num_models)):
@@ -39,7 +42,7 @@ for i in range(int(opt.num_models)):
 
     f = open(slurm_script_name, 'w+')
 
-    f.write(slurm_script.format(opt.dataset, opt.imageSize, opt.niter, experiment_name, i))
+    f.write(slurm_script.format(opt.dataset, opt.imageSize, opt.niter, opt.experiment, experiment_name, i))
 
     f.close()
 
